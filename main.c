e48a58c8fbe5dcc81bb25733d959b2450247bc46
stc_180-PC\stc_180 <whdxy12@gmail.com>
2018-05-02 16:08:34 +0900

    led좌우쉬프트

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ledshift.c
+ *
+ * Created: 2018-05-02 오후 4:06:01
+ * Author : stc_180
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
